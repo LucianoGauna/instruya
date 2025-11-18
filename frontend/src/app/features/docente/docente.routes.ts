67dba3ee6f1f74de4984ebf7b81cc79a04a1111c
Luciano Gauna <luciano.927@hotmail.com>
2025-11-17 22:45:06 -0300

    feat: add docente routing and mis materias component for docente feature

---
+++ frontend/src/app/features/docente/docente.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from "@angular/router";
+import { DocenteMateriasComponent } from "./pages/mis-materias/docente-materias.component";
+
+export const DOCENTE_ROUTES: Routes = [
+    { path: 'mis-materias', component: DocenteMateriasComponent }
+  ];
+  
---
+++ frontend/src/app/features/docente/pages/mis-materias/docente-materias.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    template: `<h1>Mis Materias (Docente)</h1>`
+  })
+  export class DocenteMateriasComponent {}
+  
