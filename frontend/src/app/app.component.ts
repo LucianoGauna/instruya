b4524af40f61e95001c2130295edadd35e7f758e
Luciano Gauna <luciano.927@hotmail.com>
2025-11-17 22:58:56 -0300

    feat: register ToastDemo and AlumnosTable components in app module

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastDemoComponent } from "./toast-demo/toast-demo.component";
+import { AlumnosTableComponent } from "./alumnos-table/alumnos-table.component";
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    ToastDemoComponent,
+    AlumnosTableComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
