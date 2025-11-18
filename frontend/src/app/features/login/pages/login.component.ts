526072805613397eb64b79bdc63a612a1cd2dabd
Luciano Gauna <luciano.927@hotmail.com>
2025-11-17 21:01:37 -0300

    feat: implement login service and enhance login component with form validation

--- frontend/src/app/features/login/pages/login.component.ts
+++ frontend/src/app/features/login/pages/login.component.ts
@@ -1,25 +1,39 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [ReactiveFormsModule, InputTextModule, ButtonModule],
   templateUrl: './login.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginPageComponent {
+  private loginService = inject(LoginService);
+  private router = inject(Router);
 
   fb = new FormBuilder();
-
   form: FormGroup = this.fb.group({
     email: [''],
-    password: ['']
+    password: [''],
   });
 
   submit() {
-    console.log(this.form.value);
+    if (this.form.invalid) return;
+
+    const { email, password } = this.form.value;
+
+    this.loginService.login({ email, password }).subscribe({
+      next: (res) => {
+        console.log('LOGIN OK:', res);
+      },
+      error: (err) => {
+        console.error('LOGIN ERROR:', err);
+      },
+    });
   }
 }
