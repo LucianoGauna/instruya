a0f015eab531f7268142fc973a91d35c00ad54fa
Luciano Gauna <luciano.927@hotmail.com>
2025-11-17 22:45:29 -0300

    feat: add routing for superadmin, admin, docente, and alumno features

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-      path: '',
-      loadChildren: () => import('../app/features/login/login.routes').then(m => m.LOGIN_ROUTES)
-    }
-  ];
-  
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/login/login.routes').then(m => m.LOGIN_ROUTES),
+  },
+  {
+    path: 'superadmin',
+    loadChildren: () =>
+      import('./features/superadmin/superadmin.routes').then(m => m.SUPERADMIN_ROUTES),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./features/admin/admin.routes').then(m => m.ADMIN_ROUTES),
+  },
+  {
+    path: 'docente',
+    loadChildren: () =>
+      import('./features/docente/docente.routes').then(m => m.DOCENTE_ROUTES),
+  },
+  {
+    path: 'alumno',
+    loadChildren: () =>
+      import('./features/alumno/alumno.routes').then(m => m.ALUMNO_ROUTES),
+  }
+];
