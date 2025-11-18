b8ed9b2746a0dac019f94dd384e6b01ee7915215
Luciano Gauna <luciano.927@hotmail.com>
2025-11-17 21:01:24 -0300

    feat: add HTTP client provider to application configuration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -6,12 +6,14 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
+    provideHttpClient(),
     providePrimeNG({ theme: {
       preset: Aura,
       options: {
