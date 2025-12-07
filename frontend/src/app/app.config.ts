7053ab4996a6befd2f709727b9d0a55d05bc4760
Luciano Gauna <luciano.927@hotmail.com>
2025-12-07 18:06:59 -0300

    feat: Implementar interceptor HTTP para adjuntar token JWT automáticamente

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,19 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     providePrimeNG({ theme: {
       preset: Aura,
       options: {
