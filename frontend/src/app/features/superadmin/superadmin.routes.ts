a2f45602e5d2bf329752b818b442f1d2d7e61c50
Luciano Gauna <luciano.927@hotmail.com>
2025-12-08 16:21:39 -0300

    feat: restructure superadmin routes to include layout component

--- frontend/src/app/features/superadmin/superadmin.routes.ts
+++ frontend/src/app/features/superadmin/superadmin.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { SuperadminDashboardComponent } from './pages/dashboard/superadmin-dashboard.component';
+import { SuperadminLayoutComponent } from './pages/layout/superadmin-layout/superadmin-layout.component';
 
 export const SUPERADMIN_ROUTES: Routes = [
-  { path: 'dashboard', component: SuperadminDashboardComponent },
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
+  {
+    path: '',
+    component: SuperadminLayoutComponent,
+    children: [
+      { path: 'dashboard', component: SuperadminDashboardComponent },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ]
+  }
 ];
