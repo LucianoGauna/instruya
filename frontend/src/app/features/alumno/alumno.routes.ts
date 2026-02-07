1b9dd3737b6dec3764de81edfe2238a45128f28e
Luciano Gauna <luciano.927@hotmail.com>
2026-02-07 20:43:25 -0300

    feat: add route for 'mis-materias' to display student's subjects

--- frontend/src/app/features/alumno/alumno.routes.ts
+++ frontend/src/app/features/alumno/alumno.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AlumnoInicioComponent } from './pages/inicio/alumno-inicio.component';
+import { AlumnoMisMateriasComponent } from './pages/mis-materias/mis-materias.component';
 
 export const ALUMNO_ROUTES: Routes = [
   { path: 'inicio', component: AlumnoInicioComponent },
+  { path: 'mis-materias', component: AlumnoMisMateriasComponent },
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
 ];
